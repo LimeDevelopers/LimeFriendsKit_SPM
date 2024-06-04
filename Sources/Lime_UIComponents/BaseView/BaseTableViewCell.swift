bee4e2676f0e5af2854c9bc5f61ccd871cdf4761
염성필 <ysp7256@limefriends.com>
2024-06-04 15:53:09 +0900

    fix BaseTableViewCell AccessControl

--- Sources/Lime_UIComponents/BaseView/BaseTableViewCell.swift
+++ Sources/Lime_UIComponents/BaseView/BaseTableViewCell.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 open class BaseTableViewCell: UITableViewCell {
-    open override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         configure()
         setConstraints()
     }
     
-    open required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
