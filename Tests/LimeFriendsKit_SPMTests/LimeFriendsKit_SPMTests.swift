ef874379c36ef7d6fdf2aa30d20121cc042dc4bc
염성필 <ysp7256@limefriends.com>
2024-06-04 15:06:56 +0900

    Initial Commit

---
+++ Tests/LimeFriendsKit_SPMTests/LimeFriendsKit_SPMTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import LimeFriendsKit_SPM
+
+final class LimeFriendsKit_SPMTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
